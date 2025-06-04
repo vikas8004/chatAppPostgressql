a83f4c10950575d75f8377bce54b117796261edb
vikas kumar <vikashkumarchaudhary635@gmail.com>
2025-06-04 18:56:56 +0530

    change in cors

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateToken = (payload, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
   });
   return token;
