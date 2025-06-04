0c349d44f22cd0e7a84ecdd6b83c652f21f8dce8
vikas kumar <vikashkumarchaudhary635@gmail.com>
2025-06-04 18:26:17 +0530

    made some change for cookie

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateToken = (payload, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
   });
   return token;
