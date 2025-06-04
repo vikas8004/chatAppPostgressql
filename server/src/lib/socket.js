a7f31d23b3f363c30db529933bf548a15101802d
vikas kumar <vikashkumarchaudhary635@gmail.com>
2025-06-04 18:44:53 +0530

    change in cors

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -6,8 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"], // Adjust this to your frontend URL
-    credentials: true
+    origin: ["http://localhost:5173", "https://chatty-2zlb.onrender.com"], // Adjust this to your frontend URL
+    credentials: true,
+    allowedHeaders: "Content-Type, Authorization"
   }
 });
 
